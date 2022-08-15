6e0c1e81fb5bdb2646744afbc3b43de3c49c2db5
Lazar Dejanovic <lazzar99@gmail.com>
2022-08-15 06:15:44 +0200

    web socket finished

--- app_crud.js
+++ app_crud.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const { sequelize } = require('./models');
+const { sequelize, Comments, Users, Restaurants } = require('./models');
 const path = require('path');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const http = require('http');
+const { Server } = require("socket.io");
 
 const users = require('./routes/users_route');
 const restaurants = require('./routes/restaurants_route');
@@ -9,6 +14,16 @@ const foods = require('./routes/foods_route');
 const comments = require('./routes/comments_route');
 
 const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:8083',
+        methods: ['GET', 'POST'],
+        credentials: true
+    },
+    allowEIO3: true
+});
 
 var corsOptions = {
     origin: ['http://localhost:8080', 'http://localhost:8082', 'http://localhost:8083'],
@@ -23,7 +38,48 @@ app.use('/admin', restaurants);
 app.use('/admin', foods);
 app.use('/admin', comments);
 
-app.listen({ port: 8081 }, async () => {
+
+
+function authSocket(comment, next) {
+    if (comment[1].token == null) {
+        next(new Error("Not authenticated"));
+    } else {
+        jwt.verify(comment[1].token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            if (err) {
+                next(new Error(err));
+            } else {
+                comment[1].user = user;
+                next();
+            }
+        });
+    }
+}
+
+io.on('connection', socket => {
+    socket.use(authSocket);
+
+    socket.on('comment', comment => {
+        Comments.create({
+            user_id: comment.user.id,
+            restaurant_id: comment.restaurant_id,
+            rate: 10,
+            content: comment.content,
+            likes: 0,
+            posted: new Date()
+        })
+            .then(row => {
+                Comments.findOne({ where: { id: row.id }, include: ['user'] })
+                    .then(cmt => io.emit('comment', JSON.stringify(cmt)))
+            })
+            .catch(err => res.status(500).json(err));
+    });
+
+    socket.on('error', err => socket.emit('error', err.message));
+});
+
+
+
+server.listen({ port: 8081 }, async () => {
     await sequelize.authenticate();
     console.log('Crud server started!');
 });
