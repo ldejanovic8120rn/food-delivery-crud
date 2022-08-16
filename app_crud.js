cc4970e8726ec8eb7f872fb0834bd09dda1e1914
Lazar Dejanovic <lazzar99@gmail.com>
2022-08-16 21:39:54 +0200

    cors options for socket

--- app_crud.js
+++ app_crud.js
@@ -18,7 +18,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: '*',
+        origin: 'https://food-delivery-gui.herokuapp.com',
         methods: ['GET', 'POST'],
         credentials: true
     },
